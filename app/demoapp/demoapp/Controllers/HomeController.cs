b2d870eeeef99a79329ec53c24d276f38ff13f64
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2023-03-28 22:36:48 +0200

    Playing with Files

--- app/demoapp/demoapp/Controllers/HomeController.cs
+++ app/demoapp/demoapp/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace demoapp.Controllers
 
         public IActionResult Privacy()
         {
+            for(int i = 0; i < 10; i++)
+            {
+                createFile("/mnt/demoappfiles/file${i}.txt");
+            }
             return View();
         }
 
@@ -32,5 +36,21 @@ namespace demoapp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        //Create a method that creates a file in an specific path
+        public void createFile(string fullFilePath)
+        {
+            //Check if the file already exists. If it does, delete it. 
+            if (System.IO.File.Exists(fullFilePath))
+            {
+                System.IO.File.Delete(fullFilePath);
+            }
+
+            //Create the file.
+            using (StreamWriter outputFile = new StreamWriter(fullFilePath))
+            {
+                var content = "New file created: ${fullFilePath}";
+                outputFile.WriteLine(content);
+            }
+        }
     }
 }
